c03cf3c13a31dbdb4606b2962bbac2a008522281
Sid <siddhantg2002@gmail.com>
2025-02-07 21:09:39 +0530

    ts-mongo-template

---
+++ ts-mongo-template/src/utils/helpers/Albums.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import { BaseData } from "./BaseData";
+
+export class AlbumData extends BaseData {
+  constructor(model: any) {
+    super(model, "Album");
+  }
+
+  public async create(req: Request, res: Response) {
+    const { userID, title } = req.body;
+
+    const album = await this.model.create({
+      data: { userID, title },
+    });
+
+    await this.clearModelCache();
+    return this.sendResponse(res, 201, "Album created successfully", album);
+  }
+
+  public async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { title } = req.body;
+
+    const album = await this.model.update({
+      where: { id },
+      data: { title },
+    });
+
+    await Promise.all([
+      this.updateRecordCache(id, album),
+      this.clearModelCache(),
+    ]);
+
+    return this.sendResponse(res, 200, "Album updated successfully", album);
+  }
+
+  public async delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    await this.model.$transaction(async (prisma: any) => {
+      await prisma.image.deleteMany({ where: { albumID: id } });
+      await prisma.album.delete({ where: { id } });
+    });
+
+    await this.clearModelCache();
+    return this.sendResponse(res, 200, "Album and images deleted successfully");
+  }
+}
